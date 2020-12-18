9038506c47f55b2e648c0ee980ff77b89cf1b374
Nicholas Donahue <nicholas.donahue@mynbps.org>
2020-12-18 17:20:04 +0000

    added changing slot values

--- App.java
+++ App.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 public class App {
   public static final Scanner Input = new Scanner(System.in);
     public static void main(String[] args) {
-      while (true) {
-        MainMenu();
-      }
+      MainMenu();
     }
 
     public static void MainMenu() {
-      //clearScreen();
+      clearScreen();
       System.out.println("WELCOME TO TIC-TAC-TOE!\n\nEnter an option's number: (ex. 1, 2, ...)");
       System.out.println("1. Play");
 
@@ -25,10 +23,65 @@ public class App {
       ArrayList<Integer> Board = new ArrayList<Integer>();
       for (int i = 0; i < 9; i++) {
         Board.add(0);
-      } 
-      drawBoard(Board);
-      System.out.println("Pick a spot");
-      Input.nextLine();
+      }
+      boolean Playing = true;
+      boolean player = true;
+      while (Playing) {
+        boolean correctSpot = false;
+        while (!correctSpot) {
+          drawBoard(Board);
+          System.out.println((player ? PlayerNames.get(0) : PlayerNames.get(1)) + " pick a spot!");
+          int slot = getSlot(Input.nextLine().toLowerCase());
+          if (Board.get(slot) == 0) {
+            if (player) {
+              Board.set(slot, 1);
+              player = false;
+            } else {
+              Board.set(slot, 2);
+               player = true;
+            }
+            correctSpot = true;
+          }
+        }
+
+        int Winner = checkForWinner(Board);
+        if (Winner == 1 || Winner == 2) {
+          Playing = false;
+          clearScreen();
+          System.out.println((PlayerNames.get(Winner - 1).toUpperCase())+ " WINS!");
+        }
+
+        if (Winner == 3) {
+          Playing = false;
+          clearScreen();
+          System.out.println(PlayerNames.get(0).toUpperCase() + " and " + PlayerNames.get(1).toUpperCase() + " WIN... BECAUSE ITS A TIE!!!!");
+        }
+      }
+    }
+    public static int getSlot(String inp) {
+      
+      String[] sp = inp.split("(?!^)");/*https://stackoverflow.com/questions/5235401/split-string-into-array-of-character-strings*/
+      int nmbr = 0;
+      if (sp[0].equals("a")) {
+        nmbr += 0;
+      }
+      if (sp[0].equals("b")) {
+        nmbr += 3;
+      }
+            if (sp[0].equals("c")) {
+        nmbr += 6;
+      }
+      nmbr += Integer.parseInt(sp[1]) - 1;
+      return nmbr;
+    }
+    public static int checkForWinner(ArrayList<Integer> brd) {
+      
+      for (int i = 0; i < brd.size(); i++) {
+        if (brd.get(i) == 0) {
+          return 0;
+        }
+      }
+      return 3;
     }
 
     public static void drawBoard(ArrayList<Integer> brd){
@@ -40,7 +93,29 @@ public class App {
       */
       String p = "";
       for (int i = 0; i < brd.size(); i++) {
-        p += "| " + brd.get(i) + " ";
+        String put = "";
+        int z = brd.get(i);
+        if (z == 1) {
+          put = "x";
+        }
+
+        if (z == 2) {
+          put = "o";
+        }
+
+        if (z == 0) {
+          if (i == 0  || i == 1 || i == 2) {
+            put = "a" + (i+1);
+          }
+          if (i == 3  || i == 4 || i == 5) {
+            put = "b" + ((i-3) + 1);
+          }
+          if (i == 6  || i == 7 || i == 8) {
+            put = "c" + ((i-6) + 1);
+          }
+        }
+
+        p += "| " + put + " ";
         if (i == 2 || i == 5 || i == 8) {
           p += "|\n";
   
